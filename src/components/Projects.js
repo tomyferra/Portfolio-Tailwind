12a1e99d179b35981fdc80e6ccf8d73b4854f7f9
tomyferra <ferra.tomy@gmail.com>
2024-03-13 20:40:25 -0300

    Finished Futbol App

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,9 +13,8 @@ function Projects() {
       <div class="grid sm:grid-cols-2 sm:gap-4 md:grid-cols-3 md:gap-4 gap-4 justify-center">
         <Project name='Crypto Converter' sitepicture={CryptoApp} summary='This web application allows users to convert Bitcoin to other currencies using the latest price data from Binance. It provides a comprehensive list of commonly traded currencies and real-time exchange rates. This app is perfect for investors and traders who need quick access to accurate market data.' url='https://tomyferra.github.io/cryptoconverter/'/>
         <Project name='WineMe' sitepicture={WineApp} summary="This web app lets users rate wines from Argentina based on personal preferences. The wine list comes from a constantly updated MongoDB database, and the app provides wine details, such as origin and grape variety. Users can explore the list and rate wines via an easy-to-use interface." url='https://wineme.onrender.com/'/>
-        <Project name='[Site in construction] Futbol Argentino Game' sitepicture={FutbolArgentino} summary='This is a simple web app game where you can try guessing which Argentina futbol team has the biggest stadium. You add one score by each guess. Next task: Add a leaderboard where all high scores will be displayed' url='https://futbolargentino.onrender.com'/>
+        <Project name='Futbol Argentino Game' sitepicture={FutbolArgentino} summary='This is a simple web app game where you can try guessing which Argentina futbol team has the biggest stadium. You add one score by each guess. Next task: Add a leaderboard where all high scores will be displayed' url='https://futbolargentinojuego.vercel.app/'/>
       </div>
-
     </div>
   )
 }
