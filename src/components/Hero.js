6e273f05c299b935db4db3543e397a5417a8c6ce
tomyferra <ferra.tomy@gmail.com>
2025-05-05 12:16:43 -0300

    updated resume

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,7 +12,7 @@ const Hero = () => {
         <div className='flex justify-center items-center'>
           <ReactTyped className='pl-2 pb-5 md:text-5hx sm:text-4xl text-xl font-bold' strings={['Python', 'React', 'Git', 'Javascript', 'Django', 'Tailwind']} typeSpeed={80} backSpeed={100} loop/>
         </div>
-        <p className='p-5 md:text-2xl text-xl md:pl-4 text-gray-300'>I am an experienced backend developer with 6 years doing Python and SQL, complemented by 4 years of front-end work in React, Tailwind and JavaScript. Aspiring to become a full-stack developer driven to fuse technical depth with creative frontend solutions. Open to relocation for the right opportunity.</p>
+        <p className='p-5 md:text-2xl text-xl md:pl-4 text-gray-300'>I am an experienced backend developer with 6 years doing Python and SQL, complemented by 4 years of front-end work in React, Tailwind and JavaScript. Aspiring to enchance my full-stack experience applying my backend and frontend stack of technologies.</p>
         <div className="flex flex-row">
           <a download='Resume Tomas Ferrari' href={resume} className='inline-flex mx-auto items-center px-3 py-2 my-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"'>Download Resume <LuDownload className='mx-2'/></a>
         </div>
